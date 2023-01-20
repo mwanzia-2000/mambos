155e6274b84b73755d664e9cba23413d3957efdf
mwanzia-2000 <123087865+mwanzia-2000@users.noreply.github.com>
2023-01-20 09:56:17 +0300

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,24 @@
+my first program
+"""
+
+print("Hello,world")
+
+name="python"
+print(True)
+
+
+
+
+
+sum = 20+5
+print(sum)
+
+
+decimal = 2.321+6.021
+print(decimal)
+
+
+
+
+
+print('Hello, world!')
